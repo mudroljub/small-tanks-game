0524c8503467aa2274d463d9b996e0c4b90b0ddd
Damjan Pavlica <mudroljub@gmail.com>
2017-02-09 01:45:34 +0100

    remove game-assets

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import {ucitaj} from 'utils'
 import sceneController from './scene/Tenkici'
 import slikaTenkPodnozje from 'slike/tenkovi/partizanski-tenk-bez-cevi.png'
-import slikaTenkCev from 'slike/tenkovi/partizanski-tenk-cev2.png'
+import slikaTenkCev from 'slike/tenkovi/partizanski-tenk-cev.png'
 import slikaGranata from 'slike/granata.gif'
 
 const slike = [slikaTenkPodnozje, slikaTenkCev, slikaGranata]
